6f1ae0c0739e563798772ff7bba529b1ea4267a2
Bob <bob@hansung.ac.kr>
2020-11-28 23:13:37 +0900

    The 4th Commit

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@
 public class Hello {
    public static void main(String[] args) {
       System.out.println("Hello, world from GIT!");
-      System.out.println("Changes after First commit!");
+      for(int i=0;i<10;i++){
+        System.out.println("2*"+i+"="+2*i);
+      }
    }
 }
